8be73510f7aaefa95985c6ef8e08eaeba9319639
Ma Hsann <maHsann@gmail.com>
2026-09-03 14:51:21 +0200

    Edited gitignore

--- src/main/java/panel/Register.java
+++ src/main/java/panel/Register.java
@@ -164,7 +164,7 @@ public class Register extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     //Created new branch Register and push
-    //remove .jar
+    //remove .jar and edited git ignore
     private void txtComfirmPasswordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtComfirmPasswordActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_txtComfirmPasswordActionPerformed
