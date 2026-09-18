a5562c8f6790cf581e144bdf901b13006b560cd3
Moe Kyaw <moekyaw@mail.arakkha.net>
2026-09-18 15:29:30 +0630

    Added Modules

--- src/main/java/main/Home.java
+++ src/main/java/main/Home.java
@@ -64,6 +64,8 @@ public class Home extends javax.swing.JFrame {
         btnUsers = new javax.swing.JButton();
         btnRegister = new javax.swing.JButton();
         btnReports = new javax.swing.JButton();
+        btnRegister1 = new javax.swing.JButton();
+        btnRegister2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
@@ -117,6 +119,14 @@ public class Home extends javax.swing.JFrame {
         btnReports.setBackground(new java.awt.Color(0, 153, 255));
         btnReports.setText("Reports");
 
+        btnRegister1.setBackground(new java.awt.Color(0, 153, 255));
+        btnRegister1.setText("Customer");
+        btnRegister1.addActionListener(this::btnRegister1ActionPerformed);
+
+        btnRegister2.setBackground(new java.awt.Color(0, 153, 255));
+        btnRegister2.setText("Inventory");
+        btnRegister2.addActionListener(this::btnRegister2ActionPerformed);
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
@@ -136,6 +146,15 @@ public class Home extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(btnRegister, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(btnRegister1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel2Layout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(btnRegister2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addContainerGap()))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -156,7 +175,14 @@ public class Home extends javax.swing.JFrame {
                 .addComponent(btnLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(btnReports, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(47, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnRegister1, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel2Layout.createSequentialGroup()
+                    .addGap(26, 26, 26)
+                    .addComponent(btnRegister2, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(449, Short.MAX_VALUE)))
         );
 
         getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 160, 510));
@@ -196,6 +222,14 @@ public class Home extends javax.swing.JFrame {
     private void btnSettingActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSettingActionPerformed
         showPanel(new setting());
     }//GEN-LAST:event_btnSettingActionPerformed
+
+    private void btnRegister1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegister1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnRegister1ActionPerformed
+
+    private void btnRegister2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegister2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnRegister2ActionPerformed
    private boolean checklogin(){
         JTextField txtName = new JTextField();
         JPasswordField txtPassword = new JPasswordField();
@@ -251,6 +285,8 @@ public class Home extends javax.swing.JFrame {
     private javax.swing.JButton btnMenu;
     private javax.swing.JButton btnProducts;
     private javax.swing.JButton btnRegister;
+    private javax.swing.JButton btnRegister1;
+    private javax.swing.JButton btnRegister2;
     private javax.swing.JButton btnReports;
     private javax.swing.JButton btnSetting;
     private javax.swing.JButton btnUsers;
